a22d15d5b4b6eca9192e6e792ed772878c9874c9
Sabrina Moccia <146776426+Samo1993@users.noreply.github.com>
2023-11-29 06:43:03 +0100

    Create array-4.js

---
+++ array-4.js
@@ -0,0 +1,33 @@
+function generateNickname(name, age) {
+    const firstname = name.slice();
+    return `${firstname}${age}`;
+}
+
+function nicknameMap(persons) {
+    const nicknames = {};
+    persons.forEach(person => {
+        const { name, age } = person;
+        nicknames[name] = generateNickname(name, age);
+    });
+    return nicknames;
+}
+
+const persons = [
+    { name: 'Paul', age: 16 },
+    { name: 'George', age: 17 },
+    { name: 'Lucas', age: 21 },
+    { name: 'Marco', age: 32 },
+    { name: 'Peter', age: 18 },
+    { name: 'Carl', age: 13 },
+    { name: 'Simon', age: 24 },
+    { name: 'Mark', age: 15 },
+    { name: 'Sandra', age: 34 },
+    { name: 'Alice', age: 28 }
+];
+
+const nicknames = nicknameMap(persons);
+console.log(persons);
+console.log(nicknames);
+
+
+/* { name: 'Paul', age: 21 } => Paul-21 */
