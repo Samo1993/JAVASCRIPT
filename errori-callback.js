03932cf93f0afb7386b0c4f8447e271df70d056f
Sabrina Moccia <146776426+Samo1993@users.noreply.github.com>
2023-11-30 09:26:42 +0100

    Create errori-callback.js

---
+++ errori-callback.js
@@ -0,0 +1,23 @@
+unction performOperation(a, b, callback) {
+    try {
+        const result = (a + b);
+        callback(null, result);
+    } catch (error) {
+        callback(error)
+    };
+};
+
+function displayResult(error, result) {
+    try {
+        if (error) {
+            throw error;
+        }
+         console.log(`Risultato:`, result);
+    } catch (error) {
+        console.error(`Si è verificato un errore:`,  error);
+    }    
+};
+
+performOperation(5, 3, displayResult);
+
+/* non ho ben capito come far risultare l'errore. So che devo aggiungere una funzione, ma non ho capito dove*/
