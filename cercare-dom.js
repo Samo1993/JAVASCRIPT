c9b8677588a560c4e37636fb7d4f1ce8eacea07d
Sabrina Moccia <146776426+Samo1993@users.noreply.github.com>
2023-12-06 12:29:14 +0100

    Update cercare-dom.js

--- cercare-dom.js
+++ cercare-dom.js
@@ -1,11 +1,26 @@
-const firstName = document.getElementById("firstName").value;
-const lastName = document.getElementById("lastName").value;
-const age = document.getElementById("age").value;
-const person = {
-    firstName: firstName,
-    lastName: lastName,
-    age: age,
-};
 const form = document.getElementById("myForm").value;
 
-form.getAttribute("data-person", JSON.stringify(person));
+function createPersonObj() {
+    const firstName = document.getElementById("firstName").value;
+    const lastName = document.getElementById("lastName").value;
+    const age = document.getElementById("age").value;
+
+        const person = {
+            firstName: firstName,
+            lastName: lastName,
+            age: age,
+        };
+    return person;
+}
+
+function addDataPersonAttribute() {
+    const person = createPersonObj();
+    const jsonData = JSON.stringify(person);
+
+    form.setAttribute("data-person", jsonData);
+}
+
+form.addEventListener("submit", function(event) {
+    event.preventDefault();
+    addDataPersonAttribute();
+});
