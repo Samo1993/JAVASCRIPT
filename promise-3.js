03ac4a57afe9c770fd1250aea713464dc0887d53
Sabrina Moccia <146776426+Samo1993@users.noreply.github.com>
2023-12-07 17:39:14 +0100

    Update promise-3.js

--- promise-3.js
+++ promise-3.js
@@ -1,20 +1,20 @@
-function fetchUserData() {
+function checkAge(age) {
     return new Promise((resolve, reject) => {
-        const user = { name: `Sabrina`, lastname: `Moccia`, age: 30};
         setTimeout(() => {
-            if (user !== 1) {  //!== => "Error: User not found" | === => "User name: undefined User lastname: undefined User age: undefined"
-                resolve(`User found`);
+            if (age >= 18) {
+                resolve("ok");
             } else {
-                reject(new Error(`User not found`))
-            };
-        }, 1000);
-    });
+                reject("ko");
+            }
+        });
+    }, 2000);
+
 }
 
-fetchUserData().then(user => {
-    console.log(`User name:`, user.name);
-    console.log(`User lastname:`, user.lastname);
-    console.log(`User age:`, user.age);
+const check = checkAge(16).then((data) => {
+    console.log(data);
 }).catch((error) => {
     console.log(error);
 });
+
+/* ho preferito semplificarla e seguire quella dell'esempio nel video */
